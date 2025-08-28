e93d758e4da558ef580396f41d5f7144f4e51ed3
Braxton Fair <hello@taxborn.com>
2025-08-27 22:42:25 -0500

    feat: introduce uranium

---
+++ hosts/uranium/configuration.nix
@@ -0,0 +1,65 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+{
+  imports = [
+    ../common
+
+    ./hardware-configuration.nix
+    ./disks.nix
+  ];
+
+  boot = {
+    kernelPackages = pkgs.linuxPackages_latest;
+    # Enable "Silent boot"
+    consoleLogLevel = 3;
+    initrd.verbose = false;
+    kernelParams = [
+      "quiet"
+      "splash"
+      "boot.shell_on_fail"
+      "udev.log_priority=3"
+      "rd.systemd.show_status=auto"
+    ];
+    loader = {
+      timeout = 0;
+      efi.canTouchEfiVariables = true;
+      grub = {
+        enable = true;
+        efiSupport = true;
+        device = "nodev";
+      };
+    };
+    plymouth = {
+      enable = true;
+    };
+  };
+
+  services.displayManager.sddm = {
+    enable = true;
+    wayland.enable = true;
+  };
+
+  networking.hostName = "uranium";
+  time.timeZone = "America/Chicago";
+
+  hardware.bluetooth.enable = true;
+  services.blueman.enable = true;
+
+  # services.libinput.enable = true;
+  programs.hyprland = {
+    enable = true;
+    withUWSM = true;
+  };
+
+  environment.systemPackages = with pkgs; [
+    wofi
+    nixfmt-rfc-style
+  ];
+
+  system.stateVersion = "25.05";
+}
