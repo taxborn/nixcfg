2ef439bb05fddd3cc2bad0d03d7e21bc24603ab8
Braxton Fair <hello@taxborn.com>
2025-09-03 19:22:34 -0500

    feat(zen): extract to new file

--- home/taxborn/applications/default.nix
+++ home/taxborn/applications/default.nix
@@ -1,17 +1,6 @@
-{ pkgs, inputs, ... }:
-
 {
   imports = [
-    inputs.zen-browser.homeModules.beta
-
     ./zed.nix
-  ];
-
-  # Web browsers
-  programs.zen-browser.enable = true;
-
-  # Additional GUI applications can be added here
-  home.packages = with pkgs; [
-    # Add other GUI applications as needed
+    ./zen.nix
   ];
 }
