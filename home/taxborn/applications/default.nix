8edfff398c2e295091d0e73509a3ebc817f08c6b
Braxton Fair <hello@taxborn.com>
2025-09-09 17:40:52 -0500

    feat: wallpaper depends on host

--- home/taxborn/applications/default.nix
+++ home/taxborn/applications/default.nix
@@ -10,4 +10,9 @@
   home.packages = with pkgs; [
     obsidian
   ];
+
+  home.sessionVariables = {
+    ELECTRON_OZONE_PLATFORM_HINT = "wayland";
+    NIXOS_OZONE_WL = 1; # fixed electron apps blurriness
+  };
 }
--- home/taxborn/hyprland/hyprpaper.nix
+++ home/taxborn/hyprland/hyprpaper.nix
@@ -1,9 +1,21 @@
+{ osConfig, ... }:
+let
+  hostname = osConfig.networking.hostName;
+
+  wallpaper =
+    if hostname == "uranium" then
+      "/home/taxborn/Media/Photos/Wallpapers/mandelbrot_gap_magenta.png"
+    else if hostname == "tungsten" then
+      "/home/taxborn/Media/Photos/Wallpapers/autumn.jpg"
+    else
+      "";
+in
 {
   services.hyprpaper = {
     enable = true;
     settings = {
-      preload = [ "/home/taxborn/Media/Photos/Wallpapers/mandelbrot_gap_magenta.png" ];
-      wallpaper = [ ",/home/taxborn/Media/Photos/Wallpapers/mandelbrot_gap_magenta.png" ];
+      preload = [ "${wallpaper}" ];
+      wallpaper = [ ",${wallpaper}" ];
     };
   };
 }
