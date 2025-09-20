209226a0a0a471f69fdd431a295ec268f66cf957
Braxton Fair <hello@taxborn.com>
2025-09-20 17:52:03 -0500

    feat: factor out carbon infra

--- hosts/carbon/caddy.nix
+++ hosts/carbon/caddy.nix
@@ -1,17 +1,11 @@
 {
   imports = [
-    ./mischief.nix
+    ./sites/mischief.nix
+    ./sites/taxborn.nix
   ];
 
   services.caddy = {
     enable = true;
-
-    virtualHosts."taxborn.com".extraConfig = ''
-      redir https://www.taxborn.com{uri} permanent
-    '';
-    virtualHosts."www.taxborn.com".extraConfig = ''
-      respond "Hello, World!"
-    '';
 
     virtualHosts."ticker.blue".extraConfig = ''
       redir https://www.ticker.blue{uri} permanent
