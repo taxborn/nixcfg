dadb8ef44a081e6851e9529fd71ffdd6e7f323b0
Braxton Fair <hello@taxborn.com>
2025-09-28 18:54:30 -0500

    feat(forgejo): create forgejo service

---
+++ hosts/carbon/sites/forgejo.nix
@@ -0,0 +1,25 @@
+{ config, ... }:
+
+{
+  services.forgejo = {
+    enable = true;
+    database.type = "postgresql";
+    lfs.enable = true;
+    settings = {
+      server = {
+        DOMAIN = "code.taxborn.com";
+        ROOT_URL = "https://${config.services.forgejo.settings.server.DOMAIN}";
+        HTTP_PORT = 8193;
+      };
+      actions = {
+        ENABLED = true;
+      };
+    };
+  };
+
+  services.caddy = {
+    virtualHosts."code.taxborn.com".extraConfig = ''
+      reverse_proxy localhost:${toString config.services.forgejo.settings.server.HTTP_PORT}
+    '';
+  };
+}
