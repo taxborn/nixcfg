1c78aff70ad14459bac709a12c4f0ea253b9e6b3
Braxton Fair <hello@taxborn.com>
2025-09-28 19:23:24 -0500

    feat: fixup reverse proxy

--- hosts/carbon/sites/mischief.nix
+++ hosts/carbon/sites/mischief.nix
@@ -10,9 +10,14 @@
       redir https://www.mischief.town{uri} permanent
     '';
 
-    # TODO: Can I dynamically get these values?
+    # Reverse proxy to paperless-ngx on helium-01 via Tailscale
     virtualHosts."docs.mischief.town".extraConfig = ''
-      reverse_proxy 100.64.1.0:21594
+      reverse_proxy http://100.64.1.0:21594 {
+        header_up Host {upstream_hostport}
+        header_up X-Real-IP {remote_host}
+        header_up X-Forwarded-For {remote_host}
+        header_up X-Forwarded-Proto {scheme}
+      }
     '';
   };
 }
