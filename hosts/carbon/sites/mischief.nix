2f815a03a254779e05fb48f948896cd69655c1c6
Braxton Fair <hello@taxborn.com>
2025-09-29 17:34:34 -0500

    feat: simplify reverse proxy

--- hosts/carbon/sites/mischief.nix
+++ hosts/carbon/sites/mischief.nix
@@ -10,13 +10,6 @@
       redir https://www.mischief.town{uri} permanent
     '';
 
-    virtualHosts."docs.mischief.town".extraConfig = ''
-      reverse_proxy http://100.64.0.0:21594 {
-        header_up Host {upstream_hostport}
-        header_up X-Real-IP {remote_host}
-        header_up X-Forwarded-For {remote_host}
-        header_up X-Forwarded-Proto {scheme}
-      }
-    '';
+    virtualHosts."docs.mischief.town".extraConfig = ''reverse_proxy http://100.64.0.0:21594'';
   };
 }
