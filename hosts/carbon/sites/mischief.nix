1162bb5da32731ae54fdafde1705fe8665dbef20
Braxton Fair <hello@taxborn.com>
2025-09-28 20:41:23 -0500

    feat: restrict paperless to tailscale

--- hosts/carbon/sites/mischief.nix
+++ hosts/carbon/sites/mischief.nix
@@ -11,12 +11,26 @@
     '';
 
     # Reverse proxy to paperless-ngx on helium-01 via Tailscale
+    # Only accessible from Tailscale network (100.64.0.0/10)
     virtualHosts."docs.mischief.town".extraConfig = ''
-      reverse_proxy http://100.64.1.0:21594 {
-        header_up Host {upstream_hostport}
-        header_up X-Real-IP {remote_host}
-        header_up X-Forwarded-For {remote_host}
-        header_up X-Forwarded-Proto {scheme}
+      # Define matcher for Tailscale IP range
+      @tailscale {
+        remote_ip 100.64.0.0/10
+      }
+
+      # Only allow Tailscale clients
+      handle @tailscale {
+        reverse_proxy http://100.64.0.0:21594 {
+          header_up Host {upstream_hostport}
+          header_up X-Real-IP {remote_host}
+          header_up X-Forwarded-For {remote_host}
+          header_up X-Forwarded-Proto {scheme}
+        }
+      }
+
+      # Deny all other traffic
+      handle {
+        respond "Access denied: Tailscale required" 403
       }
     '';
   };
