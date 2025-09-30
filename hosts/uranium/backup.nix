2df8ad4cbb9bb9fbceb4f74badf3c481d675b50d
Braxton Fair <hello@taxborn.com>
2025-09-29 21:15:22 -0500

    feat(backups): re-enable carbon and uranium

--- hosts/uranium/backup.nix
+++ hosts/uranium/backup.nix
@@ -41,23 +41,23 @@ in
     startAt = "daily";
   };
 
-  # services.borgbackup.jobs."uranium-helium" = {
-  #   paths = [
-  #     "/home"
-  #     "/var/lib"
-  #     "/etc"
-  #   ];
-  #   exclude = commonExcludes;
-  #   repo = "ssh://taxborn@100.64.1.0//mnt/hdd/borg-repos/uranium";
-  #   encryption = {
-  #     mode = "repokey-blake2";
-  #     passCommand = "cat /root/borg/passphrase";
-  #   };
-  #   environment = {
-  #     BORG_RSH = "ssh -i /root/borg/ssh_key";
-  #   };
-  #   extraCreateArgs = "--verbose --stats --list --checkpoint-interval 600";
-  #   compression = "auto,zstd";
-  #   startAt = "daily";
-  # };
+  services.borgbackup.jobs."uranium-helium" = {
+    paths = [
+      "/home"
+      "/var/lib"
+      "/etc"
+    ];
+    exclude = commonExcludes;
+    repo = "ssh://taxborn@100.64.1.0//mnt/hdd/borg-repos/uranium";
+    encryption = {
+      mode = "repokey-blake2";
+      passCommand = "cat /root/borg/passphrase";
+    };
+    environment = {
+      BORG_RSH = "ssh -i /root/borg/ssh_key";
+    };
+    extraCreateArgs = "--verbose --stats --list --checkpoint-interval 600";
+    compression = "auto,zstd";
+    startAt = "daily";
+  };
 }
