46e7558ba27cb4bf406fd93a75fcfb67d8f1e809
Braxton Fair <hello@taxborn.com>
2025-09-29 21:46:27 -0500

    chore: cleanup helium config

--- hosts/helium-01/configuration.nix
+++ hosts/helium-01/configuration.nix
@@ -13,11 +13,6 @@
   networking.hostName = "helium-01";
   time.timeZone = "America/Chicago";
 
-  users.users.root.openssh.authorizedKeys.keys = [
-    # Temp for now, don't want this in root forever.
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOf8rn+JzRmVc6/4xKOJ4MrmId4xxpYPEgvbCrK18U+N yubikey"
-  ];
-
   # https://github.com/NixOS/nixpkgs/issues/180175
   systemd.services.NetworkManager-wait-online.enable = false;
   systemd.services.tailscaled.after = [ "systemd-networkd-wait-online.service" ];
@@ -28,8 +23,8 @@
     ntfs3g
   ];
 
-  services.openssh.enable = true;
-  networking.firewall.allowedTCPPorts = [ 22 ]; # might already be allowed? just to be safe.
+  # services.openssh.enable = true;
+  # networking.firewall.allowedTCPPorts = [ 22 ]; # might already be allowed? just to be safe.
 
   system.stateVersion = "25.05";
 }
