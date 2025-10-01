db833729d28bcc19a799e187661fb64f69d08803
Braxton Fair <hello@taxborn.com>
2025-09-30 20:51:54 -0500

    chore: add tungsten root key

--- hosts/common/users/taxborn.nix
+++ hosts/common/users/taxborn.nix
@@ -9,6 +9,7 @@ let
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOf8rn+JzRmVc6/4xKOJ4MrmId4xxpYPEgvbCrK18U+N yubikey"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILhCoSFBNavdXRZ8sH1dzGwLO90BfYLNwbbu0yxVgxjf root@carbon"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO3R84Qt7+WC/pplvm+TNLEreIiBQTiAtbLiYYhucvkL root@helium-01"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFgQtojwvTH5KeMYWNRyIwFtD30Oiepv8qbTUf8kCMLg root@tungsten"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK2Bj6eF8CLcdxFQZZc7pxpSf/53mbPKRqjJQnK2SXiA root@uranium"
   ];
 in
