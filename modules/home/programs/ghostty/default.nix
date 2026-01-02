d7c57361c670728054b6c453a5726c492c53ed94
Braxton Fair <hello@taxborn.com>
2026-01-02 08:31:30 -0600

    style(ghostty): switch to catppuccin

--- modules/home/programs/ghostty/default.nix
+++ modules/home/programs/ghostty/default.nix
@@ -11,10 +11,13 @@
       enable = true;
 
       settings = {
-        theme = "Adwaita Dark";
-        window-padding-x = 6;
+        font-family = "Atkinson Hyperlegible Mono";
+        window-padding-x = 4;
         window-padding-y = 4;
+        theme = "catppuccin-mocha.conf";
       };
     };
+
+    home.file.".config/ghostty/themes/catppuccin-mocha.conf".source = ./catppuccin-mocha.conf;
   };
 }
