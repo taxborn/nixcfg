8edc9390fb9e652d7d41a60604f194b97dd584fa
Braxton Fair <hello@taxborn.com>
2026-01-25 20:35:04 -0600

    chore: clean up the proxy file for carbon

--- hosts/carbon/proxy.nix
+++ hosts/carbon/proxy.nix
@@ -1,26 +1,53 @@
 { config, ... }:
+let
+  # Short alias for the network map
+  net = config.mySnippets.mischief-town.networkMap;
+
+  # Helper function for simple reverse proxy to localhost
+  localProxy = service: ''
+    reverse_proxy localhost:${toString service.port}
+  '';
+
+  # Helper function for reverse proxy to a specific host
+  remoteProxy = host: service: ''
+    reverse_proxy http://${host}:${toString service.port}
+  '';
+
+  # Helper function for encoded reverse proxy with common headers
+  encodedProxy = upstream: port: ''
+    encode zstd gzip
+    reverse_proxy ${upstream}:${toString port} {
+      header_up X-Real-IP {remote_host}
+      header_up X-Forwarded-For {remote_host}
+      header_up X-Forwarded-Proto {scheme}
+    }
+  '';
+
+  # Tailscale IP for services on another machine
+  heliumTailscaleIP = "100.64.1.0";
+in
 {
   services.caddy.virtualHosts = {
     # Personal Website proxies
-    "taxborn.com".extraConfig = ''
-      redir https://www.taxborn.com{uri} permanent
-    '';
-    "www.taxborn.com".extraConfig = ''
-      reverse_proxy localhost:${toString config.mySnippets.mischief-town.networkMap.taxborn-com.port}
-    '';
     "braxtonfair.com".extraConfig = ''
       redir https://www.braxtonfair.com{uri} permanent
     '';
     "www.braxtonfair.com".extraConfig = ''
       redir https://www.taxborn.com{uri} permanent
     '';
-
-    # mischief.town proxies
-    "mischief.town".extraConfig = ''
-      redir https://${config.mySnippets.mischief-town.networkMap.glance.vHost}{uri} permanent
+    "taxborn.com".extraConfig = ''
+      redir https://www.taxborn.com{uri} permanent
     '';
-    ${config.mySnippets.mischief-town.networkMap.pds.vHost}.extraConfig = ''
-      reverse_proxy localhost:${toString config.mySnippets.mischief-town.networkMap.pds.port}
+    "www.taxborn.com".extraConfig = localProxy net.taxborn-com;
+
+    # mischief.town - Main redirect
+    "mischief.town".extraConfig = ''
+      redir https://${net.glance.vHost}{uri} permanent
+    '';
+
+    # mischief.town - Bluesky PDS (with age assurance workarounds)
+    ${net.pds.vHost}.extraConfig = ''
+      reverse_proxy localhost:${toString net.pds.port}
 
       handle /xrpc/app.bsky.unspecced.getAgeAssuranceState {
         header content-type "application/json"
@@ -35,6 +62,7 @@
         header access-control-allow-origin "*"
         respond `{"regions":[]}` 200
       }
+
       handle /xrpc/app.bsky.ageassurance.getState {
         header content-type "application/json"
         header access-control-allow-headers "authorization,dpop,atproto-accept-labelers,atproto-proxy"
@@ -42,18 +70,16 @@
         respond `{"state":{"lastInitiatedAt":"2025-07-14T14:22:43.912Z","status":"assured","access":"full"},"metadata":{"accountCreatedAt":"2022-11-17T00:35:16.391Z"}}` 200
       }
     '';
-    ${config.mySnippets.mischief-town.networkMap.glance.vHost}.extraConfig = ''
-      reverse_proxy localhost:${toString config.mySnippets.mischief-town.networkMap.glance.port}
-    '';
-    ${config.mySnippets.mischief-town.networkMap.tangled-knot.vHost}.extraConfig = ''
-      reverse_proxy localhost:${toString config.mySnippets.mischief-town.networkMap.tangled-knot.port}
-    '';
-    ${config.mySnippets.mischief-town.networkMap.tangled-spindle.vHost}.extraConfig = ''
-      reverse_proxy localhost:${toString config.mySnippets.mischief-town.networkMap.tangled-spindle.port}
-    '';
-    ${config.mySnippets.mischief-town.networkMap.paperless.vHost}.extraConfig =
-      "reverse_proxy http://100.64.1.0:${toString config.mySnippets.mischief-town.networkMap.paperless.port}";
-    ${config.mySnippets.mischief-town.networkMap.forgejo.vHost}.extraConfig = ''
+
+    # mischief.town - Simple local proxies
+    ${net.glance.vHost}.extraConfig = localProxy net.glance;
+    ${net.tangled-knot.vHost}.extraConfig = localProxy net.tangled-knot;
+    ${net.tangled-spindle.vHost}.extraConfig = localProxy net.tangled-spindle;
+
+    # mischief.town - Services on Tailscale network (remote host)
+    ${net.paperless.vHost}.extraConfig = remoteProxy heliumTailscaleIP net.paperless;
+
+    ${net.forgejo.vHost}.extraConfig = ''
       encode zstd gzip
 
       @uploads method POST PUT
@@ -61,33 +87,15 @@
         request_body { max_size 2GB }
       }
 
-      reverse_proxy localhost:${toString config.mySnippets.mischief-town.networkMap.forgejo.port}  {
+      reverse_proxy localhost:${toString net.forgejo.port} {
         header_up X-Real-Ip {remote_host}
       }
     '';
-    ${config.mySnippets.mischief-town.networkMap.vaultwarden.vHost}.extraConfig = ''
-      encode zstd gzip
-      reverse_proxy localhost:${toString config.mySnippets.mischief-town.networkMap.vaultwarden.port}  {
-        header_up X-Real-IP {remote_host}
-        header_up X-Forwarded-For {remote_host}
-        header_up X-Forwarded-Proto {scheme}}
-      }
-    '';
-    ${config.mySnippets.mischief-town.networkMap.immich.vHost}.extraConfig = ''
-      encode zstd gzip
-      reverse_proxy http://100.64.1.0:${toString config.mySnippets.mischief-town.networkMap.immich.port}  {
-        header_up X-Real-IP {remote_host}
-        header_up X-Forwarded-For {remote_host}
-        header_up X-Forwarded-Proto {scheme}}
-      }
-    '';
-    ${config.mySnippets.mischief-town.networkMap.copyparty.vHost}.extraConfig = ''
-      encode zstd gzip
-      reverse_proxy http://100.64.1.0:${toString config.mySnippets.mischief-town.networkMap.copyparty.port}  {
-        header_up X-Real-IP {remote_host}
-        header_up X-Forwarded-For {remote_host}
-        header_up X-Forwarded-Proto {scheme}
-      }
-    '';
+
+    ${net.vaultwarden.vHost}.extraConfig = encodedProxy "localhost" net.vaultwarden.port;
+
+    # mischief.town - Encoded proxies with headers (Tailscale)
+    ${net.immich.vHost}.extraConfig = encodedProxy "http://${heliumTailscaleIP}" net.immich.port;
+    ${net.copyparty.vHost}.extraConfig = encodedProxy "http://${heliumTailscaleIP}" net.copyparty.port;
   };
 }
