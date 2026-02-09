b564a6aa7a31989be7533f2cde4c33a31177a0a4
Braxton Fair <hello@taxborn.com>
2026-02-09 16:39:11 -0600

    chore: remove gatekeeper

--- modules/nixos/services/atproto/default.nix
+++ modules/nixos/services/atproto/default.nix
@@ -38,18 +38,5 @@
         ];
       };
     };
-
-    services.pds-gatekeeper = {
-      enable = true;
-
-      settings = {
-        GATEKEEPER_PORT = config.mySnippets.mischief-town.networkMap.pds.gatekeeperPort;
-
-        PDS_BASE_URL = "http://127.0.0.1:${toString config.mySnippets.mischief-town.networkMap.pds.port}";
-        GATEKEEPER_TRUST_PROXY = "true";
-
-        PDS_ENV_LOCATION = config.age.secrets.pds.path;
-      };
-    };
   };
 }
