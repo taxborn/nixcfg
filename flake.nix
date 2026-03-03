3517e89b29cab1140bf936cf4bcd76df9d4d1bc4
Braxton Fair <hello@taxborn.com>
2026-03-03 17:02:29 -0600

    chore: remove gatekeeper input

--- flake.nix
+++ flake.nix
@@ -25,13 +25,6 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    tgirlpkgs = {
-      url = "github:tgirlcloud/pkgs";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-      };
-    };
-
     zen-browser = {
       url = "github:0xc000022070/zen-browser-flake";
       inputs.nixpkgs.follows = "nixpkgs";
