b7676c522144e03e5982439c6c95566124d83015
Braxton Fair <hello@taxborn.com>
2026-03-03 18:23:34 -0600

    fix: multiple swaps were not needed

--- modules/nixos/profiles/swap/default.nix
+++ modules/nixos/profiles/swap/default.nix
@@ -7,25 +7,11 @@
   options.myNixOS.profiles.swap.enable = lib.mkEnableOption "swap and oomd configurations";
 
   config = lib.mkIf config.myNixOS.profiles.swap.enable {
-    swapDevices = [
-      {
-        device = "/.swap";
-        priority = 0;
-        randomEncryption.enable = true;
-        size = 8192;
-      }
-    ];
-
     systemd.oomd = {
       enable = true;
       enableRootSlice = true;
       enableSystemSlice = true;
       enableUserSlices = true;
-    };
-
-    zramSwap = {
-      enable = true;
-      algorithm = "zstd";
     };
   };
 }
