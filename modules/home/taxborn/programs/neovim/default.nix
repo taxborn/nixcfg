1b927270a184b6dcb3a9d7c4adf34b2fc5a026f6
Braxton Fair <hello@taxborn.com>
2026-03-06 13:49:45 -0600

    feat(nvim): work on config

--- modules/home/taxborn/programs/neovim/default.nix
+++ modules/home/taxborn/programs/neovim/default.nix
@@ -1,6 +1,7 @@
 {
   lib,
   config,
+  pkgs,
   ...
 }:
 {
@@ -16,47 +17,20 @@
       vimAlias = true;
       defaultEditor = true;
 
-      initLua = ''
-        -- line numbers
-        vim.opt.number = true
-        vim.opt.relativenumber = true
+      # runtime deps: ripgrep/fd for telescope, gcc+make for telescope-fzf-native, git for lazy.nvim
+      extraPackages = with pkgs; [
+        ripgrep
+        fd
+        gcc
+        gnumake
+        unzip
+        git
+        tree-sitter
+        lua-language-server
+        stylua
+      ];
 
-        -- indentation
-        vim.opt.tabstop = 4
-        vim.opt.shiftwidth = 4
-        vim.opt.expandtab = true
-        vim.opt.smartindent = true
-
-        -- search
-        vim.opt.ignorecase = true
-        vim.opt.smartcase = true
-        vim.opt.hlsearch = false
-        vim.opt.incsearch = true
-
-        -- appearance
-        vim.opt.termguicolors = true
-        vim.opt.signcolumn = "yes"
-        vim.opt.scrolloff = 8
-        vim.opt.wrap = false
-
-        -- behavior
-        vim.opt.splitbelow = true
-        vim.opt.splitright = true
-        vim.opt.undofile = true
-
-        -- leader key
-        vim.g.mapleader = " "
-        vim.g.maplocalleader = " "
-
-        -- keymaps
-        vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = "Open netrw" })
-        vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Scroll down and center" })
-        vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Scroll up and center" })
-        vim.keymap.set("n", "n", "nzzzv", { desc = "Next search result centered" })
-        vim.keymap.set("n", "N", "Nzzzv", { desc = "Prev search result centered" })
-        vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
-        vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
-      '';
+      initLua = builtins.readFile ./init.lua;
     };
   };
 }
