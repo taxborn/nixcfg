34cd579245b89b124421e435ebfaf89ac0874e7d
Braxton Fair <hello@taxborn.com>
2026-03-09 09:02:59 -0500

    fix: update caddy hash

--- modules/nixos/services/caddy/default.nix
+++ modules/nixos/services/caddy/default.nix
@@ -29,7 +29,7 @@
 
         package = pkgs.caddy.withPlugins {
           plugins = [ "github.com/tailscale/caddy-tailscale@v0.0.0-20250508175905-642f61fea3cc" ];
-          hash = "sha256-VNQUlHz2Gr+tcT4ui3kd+YvlaFZiXSy7CIdwovoDVjQ=";
+          hash = "sha256-wrG0IbxZqR6UQxLImCnfra0tl9tSBk75E5WxgtVucsI=";
         };
       };
 
