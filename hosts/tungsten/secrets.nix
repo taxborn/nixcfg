bc331f69bf598464ee0fc65c707e5ca1e0a386e0
Braxton Fair <hello@taxborn.com>
2026-04-28 08:42:50 -0500

    chore: add age identityPaths to tungsten

--- hosts/tungsten/secrets.nix
+++ hosts/tungsten/secrets.nix
@@ -1,6 +1,13 @@
 { self, ... }:
 {
-  age.secrets = {
-    tailscaleAuthKey.file = "${self.inputs.secrets}/tailscale/auth.age";
+  age = {
+    identityPaths = [
+      "/etc/ssh/ssh_host_ed25519_key"
+      "/home/taxborn/.config/age/yubikey-identity.txt"
+    ];
+
+    secrets = {
+      tailscaleAuthKey.file = "${self.inputs.secrets}/tailscale/auth.age";
+    };
   };
 }
