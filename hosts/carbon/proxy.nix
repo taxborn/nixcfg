6b1fd5512757aa2d1c8a3d2e3e53ce0a44836849
Braxton Fair <hello@taxborn.com>
2026-05-03 09:40:44 -0500

    chore(pds): remove age assurance
    I keep getting the birthday prompts, not sure what I am doing wrong,
    will experiment with this later

--- hosts/carbon/proxy.nix
+++ hosts/carbon/proxy.nix
@@ -12,18 +12,8 @@ in
       }
     '';
 
-    # https://gist.github.com/mary-ext/6e27b24a83838202908808ad528b3318#method-5-self-hosted-pds
-    # last checked 2026-05-01
     ${networkMap.pds.domain}.extraConfig = ''
       encode zstd gzip
-
-      handle /xrpc/app.bsky.ageassurance.getState {
-        header content-type "application/json"
-        header access-control-allow-headers "authorization,dpop,atproto-accept-labelers,atproto-proxy"
-        header access-control-allow-origin "*"
-        respond `{"state":{"lastInitiatedAt":"2025-07-14T14:22:43.912Z","status":"assured","access":"full"},"metadata":{"accountCreatedAt":"2022-11-17T00:35:16.391Z"}}` 200
-      }
-
       reverse_proxy http://localhost:${toString networkMap.pds.port}
     '';
   };
