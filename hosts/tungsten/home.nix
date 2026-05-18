05f53842d71fc4bd4f59f313cd66bc4ef9fa0a29
Braxton Fair <hello@taxborn.com>
2026-05-18 15:28:36 -0500

    fix(hypr): remove env variables

--- hosts/tungsten/home.nix
+++ hosts/tungsten/home.nix
@@ -7,12 +7,5 @@
     imports = [
       self.homeModules.profile-workstation
     ];
-
-    config = {
-      wayland.windowManager.hyprland.settings.env = [
-        "GDK_SCALE,2"
-        "XCURSOR_SIZE,48"
-      ];
-    };
   };
 }
