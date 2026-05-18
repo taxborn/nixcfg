ff1e214a5ea744492acc01f3a698a4f27976d294
Braxton Fair <hello@taxborn.com>
2026-05-18 11:10:04 -0500

    feat: remove agenix secret from tungsten

--- hosts/tungsten/secrets.nix
+++ hosts/tungsten/secrets.nix
@@ -5,9 +5,5 @@
       "/etc/ssh/ssh_host_ed25519_key"
       "/home/taxborn/.config/age/yubikey-identity.txt"
     ];
-
-    secrets = {
-      tailscaleAuthKey.file = "${self}/secrets/tailscale/auth.age";
-    };
   };
 }
