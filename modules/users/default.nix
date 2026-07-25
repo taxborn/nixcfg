d0738a87bdb23b6ed89ba25af7deabb5cb3678d8
Braxton Fair <hello@taxborn.com>
2026-07-24 22:35:58 -0500

    feat: define taxborn user
    my account!

--- modules/users/default.nix
+++ modules/users/default.nix
@@ -1,1 +1,22 @@
-{ }
+{ pkgs, ... }:
+
+{
+  programs.fish.enable = true;
+
+  users = {
+    users.taxborn = {
+      description = "Braxton";
+      uid = 1000;
+      isNormalUser = true;
+      hashedPassword = "$y$j9T$A0TNjHtgoYuPaVVUDMTg1/$c2X6a5BbYruE.WN0ko5uE3O.FTGDFeEWjFDxwL4YS28";
+      extraGroups = [
+        "wheel"
+      ];
+      openssh.authorizedKeys.keyFiles = [
+        ../../secrets/publicKeys/taxborn_yubikey_ssh.pub
+      ];
+    };
+    defaultUserShell = pkgs.fish;
+    mutableUsers = false;
+  };
+}
