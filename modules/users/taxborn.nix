10a9d4389f4604e470d386ee7cc4c695215e7bf4
Braxton Fair <hello@taxborn.com>
2026-08-02 07:26:18 -0500

    fix(taxborn): add to networkmanager group

--- modules/users/taxborn.nix
+++ modules/users/taxborn.nix
@@ -10,6 +10,8 @@
       isNormalUser = true;
       hashedPassword = "$y$j9T$A0TNjHtgoYuPaVVUDMTg1/$c2X6a5BbYruE.WN0ko5uE3O.FTGDFeEWjFDxwL4YS28";
       extraGroups = [
+        "networkmanager"
+        "video"
         "wheel"
       ];
       openssh.authorizedKeys.keyFiles = [
